fabe55a9231999b622fe1627cc152977e10ddf18
Suppasit <monitorman_sc@hotmail.com>
2024-07-17 08:55:53 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+**SwiftUI in Practice**
+
+This repository contains the file I follow along with the video course from Nick [here](https://www.youtube.com/playlist?list=PLwvDm4VfkdpiT7mKzjxfCYn_zaMC3Fmkz).
---
+++ SwiftfulSwiftUIInPractice/SwiftfulSwiftUIInPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSwiftUIInPracticeApp.swift
+//  SwiftfulSwiftUIInPractice
+//
+//  Created by Suppasit chuwatsawat on 17/7/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSwiftUIInPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
