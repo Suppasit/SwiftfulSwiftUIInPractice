e510d7acc86baaa44c5cf54d6575dda12991e529
Suppasit <monitorman_sc@hotmail.com>
2024-07-26 09:43:31 +0700

    Rebuild Spotify (Part 2/5)
    1. Category Cell
    2. New Release Cell
    3. Recent Cell

---
+++ SwiftfulSwiftUIInPractice/Spotify/Components/SpotifyRecentCell.swift
@@ -0,0 +1,47 @@
+//
+//  SpotifyRecentCell.swift
+//  SwiftfulSwiftUIInPractice
+//
+//  Created by Suppasit chuwatsawat on 25/7/2567 BE.
+//
+
+import SwiftUI
+
+struct SpotifyRecentCell: View {
+    
+    var imageName: String = Constants.randomImage
+    var title: String = "Some random title"
+
+    var body: some View {
+        HStack {
+            ImageLoaderView(urlString: imageName)
+                .frame(width: 55, height: 55)
+            
+            Text(title)
+                .font(.callout)
+                .fontWeight(.semibold)
+                .lineLimit(2)
+        }
+        .padding(.trailing, 8)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .themeColors(isSelected: false)
+        .cornerRadius(6)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        
+        VStack {
+            HStack {
+                SpotifyRecentCell()
+                SpotifyRecentCell()
+            }
+            HStack {
+                SpotifyRecentCell()
+                SpotifyRecentCell()
+            }
+        }
+    }
+}
--- SwiftfulSwiftUIInPractice/Spotify/Core/SpotifyHomeView.swift
+++ SwiftfulSwiftUIInPractice/Spotify/Core/SpotifyHomeView.swift
@@ -6,11 +6,13 @@
 //
 
 import SwiftUI
+import SwiftfulUI
 
 struct SpotifyHomeView: View {
     
     @State private var currentUser: User? = nil
     @State private var selectedCategory: Category? = nil
+    @State private var products: [Product] = []
 
     var body: some View {
         ZStack {
@@ -19,6 +21,15 @@ struct SpotifyHomeView: View {
             ScrollView(.vertical) {
                 LazyVStack(spacing: 1, pinnedViews: [.sectionHeaders]) {
                     Section {
+                        VStack(spacing: 16) {
+                            recentsSection
+                            
+                            if let product = products.first {
+                                newReleaseSection(product: product)
+                            }
+                        }
+                        .padding(.horizontal, 16)
+                        
                         ForEach(0..<20) { _ in
                             Rectangle()
                                 .fill(Color.red)
@@ -37,13 +48,13 @@ struct SpotifyHomeView: View {
         .task {
             await getData()
         }
-        .toolbarVisibility(.hidden, for: .navigationBar)
+        .toolbar(.hidden, for: .navigationBar)
     }
     
     private func getData() async {
         do {
             currentUser = try await DatabaseHelper().getUsers().first
-            // products = try await DatabaseHelper().getProducts()
+            products = try await Array(DatabaseHelper().getProducts().prefix(8))
             print("🎉 succefully get the data!")
         } catch {
             print(error.localizedDescription)
@@ -82,6 +93,33 @@ struct SpotifyHomeView: View {
         .padding(.vertical, 24)
         .padding(.leading, 8)
         .background(Color.spotifyBlack)
+    }
+    
+    private var recentsSection: some View {
+        NonLazyVGrid(columns: 2, alignment: .center, spacing: 10, items: products) { product in
+            if let product {
+                SpotifyRecentCell(
+                    imageName: product.firstImage,
+                    title: product.title
+                )
+            }
+        }
+    }
+    
+    private func newReleaseSection(product: Product) -> some View {
+        SpotifyNewReleaseCell(
+            imageName: product.firstImage,
+            headline: product.brand,
+            subheadline: product.category,
+            title: product.title,
+            subtitle: product.description,
+            onAddToPlaylistPress: {
+                print("onAddToPlaylistPress")
+            },
+            onPlayPress: {
+                print("onPlayPress")
+            }
+        )
     }
 }
 
