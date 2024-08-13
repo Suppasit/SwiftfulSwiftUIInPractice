245624ee0b086c51488e418893889d99edb193d1
Suppasit <monitorman_sc@hotmail.com>
2024-08-13 09:41:27 +0700

    Rebuild Bumble in SwiftUI (Part 1/5)

---
+++ SwiftfulSwiftUIInPractice/Bumble/Core/BumbleHomeView.swift
@@ -0,0 +1,68 @@
+//
+//  BumbleHomeView.swift
+//  SwiftfulSwiftUIInPractice
+//
+//  Created by Suppasit chuwatsawat on 12/8/2567 BE.
+//
+
+import SwiftUI
+
+struct BumbleHomeView: View {
+    
+    @State var filters: [String] = ["Everyone", "Trending"]
+    @AppStorage("bumble_home_filter") private var selectedFilter: String = "Everyone"
+    
+    var body: some View {
+        ZStack {
+            Color.bumbleBackgroundYellow.ignoresSafeArea()
+            
+            VStack(spacing: 12) {
+                header
+                
+                BumbleFilterView(options: filters, selection: $selectedFilter)
+                    .background(Divider(), alignment: .bottom)
+                
+                Spacer()
+            }
+            .padding(8)
+        }
+    }
+    
+    private var header: some View {
+        HStack(spacing: 0) {
+            HStack(spacing: 0) {
+                Image(systemName: "line.horizontal.3")
+                    .padding(8)
+                    .background(Color.black.opacity(0.001))
+                    .onTapGesture {
+                    }
+                
+                Image(systemName: "arrow.uturn.left")
+                    .padding(8)
+                    .background(Color.black.opacity(0.001))
+                    .onTapGesture {
+                    }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Text("bumble")
+                .font(.title)
+                .foregroundStyle(.bumbleYellow)
+                .frame(maxWidth: .infinity, alignment: .center)
+            
+            Image(systemName: "slider.horizontal.3")
+                .padding(8)
+                .background(Color.black.opacity(0.001))
+                .onTapGesture {
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+        }
+        .font(.title2)
+        .fontWeight(.medium)
+        .foregroundStyle(.bumbleBlack)
+    }
+}
+
+#Preview {
+    BumbleHomeView()
+}
