fe9eff17d70683b9d3fb8095f2c83175b47bf6cf
Suppasit <monitorman_sc@hotmail.com>
2024-08-30 08:32:28 +0700

    Rebuild Netflix in SwiftUI (Part 2/6)

--- SwiftfulSwiftUIInPractice/Netflix/Core/NetflixHomeView.swift
+++ SwiftfulSwiftUIInPractice/Netflix/Core/NetflixHomeView.swift
@@ -6,16 +6,59 @@
 //
 
 import SwiftUI
+import SwiftfulUI
 
 struct NetflixHomeView: View {
     
     @State private var filters = FilterModel.mockAray
     @State private var selectedFilter: FilterModel? = nil
+    @State private var fullHeaderSize: CGSize = .zero
+    
+    @State private var heroProduct: Product? = nil
+    @State private var currentUser: User? = nil
+    @State private var productRows: [ProductRow] = []
     
     var body: some View {
-        ZStack {
+        ZStack(alignment: .top) {
             Color.netflixBlack.ignoresSafeArea()
             
+            ScrollView(.vertical) {
+                VStack(spacing: 4) {
+                    Rectangle()
+                        .opacity(0)
+                        .frame(height: fullHeaderSize.height)
+                    
+                    if let heroProduct {
+                        NetflixHeroCell(
+                            imageName: heroProduct.firstImage,
+                            isNetflixFilm: true,
+                            title: heroProduct.title,
+                            categories: [
+                                heroProduct.category.capitalized,
+                                heroProduct.brand ?? "XXX"
+                            ],
+                            onBackgroundPressed: {
+                                
+                            },
+                            onPlayPressed: {
+                                
+                            },
+                            onMyListPressed: {
+                                
+                            }
+                        )
+                        .padding(24)
+                    }
+                    
+                    ForEach(0..<20) { _ in
+                        Rectangle()
+                            .fill(Color.red)
+                            .frame(height: 200)
+                    }
+                }
+            }
+            .scrollIndicators(.hidden)
+
             VStack(spacing: 0) {
                 header
                     .padding(.horizontal, 16)
@@ -28,11 +71,40 @@ struct NetflixHomeView: View {
                         selectedFilter = nil
                     }
                     .padding(.top, 16)
-                
-                Spacer()
             }
+            .background(Color.blue)
+            .readingFrame { frame in
+                fullHeaderSize = frame.size
+            }
+            
         }
         .foregroundStyle(.netflixWhite)
+        .task {
+            await getData()
+        }
+        .toolbarVisibility(.hidden, for: .navigationBar)
+    }
+
+    private func getData() async {
+        guard productRows.isEmpty else { return }
+        
+        do {
+            currentUser = try await DatabaseHelper().getUsers().first
+            let products = try await DatabaseHelper().getProducts()
+            heroProduct = products.first
+            
+            var rows: [ProductRow] = []
+            let allBrands = Set(products.map { $0.brand })
+            for brand in allBrands {
+                guard let brand else { return }
+                rows.append(ProductRow(title: brand.capitalized, products: products))
+            }
+            productRows = rows
+            
+            print("🎉 succefully get the data!")
+        } catch {
+            print(error.localizedDescription)
+        }
     }
     
     private var header: some View {
